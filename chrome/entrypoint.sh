e91172ec5c07511fc63f9a55617dacb202da3c47
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-05-16 19:31:17 -0400

    Add nightwatch and refactor chrome container

---
+++ chrome/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nohup google-chrome \
+  --no-sandbox \
+  --headless \
+  --disable-gpu \
+  --user-data-dir=/tmp \
+  --remote-debugging-port=9222 \
+  --remote-debugging-address=0.0.0.0 &
+
+chromedriver --whitelisted-ips=''
