97b77dedd1b0226a1eb99ef12278e22900e6e268
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-05-19 11:55:13 -0400

    Add webdriverio

---
+++ webdriverio/specs.js
@@ -0,0 +1,16 @@
+const { browser } = global;
+
+describe('DuckDuckGo search', function() {
+    it('searches for WebdriverIO', function() {
+        browser.url('https://duckduckgo.com/');
+        
+        const searchInputElement = browser.element('#search_form_input_homepage');
+        searchInputElement.setValue('WebdriverIO');
+
+        const searchButtonElement = browser.element('#search_button_homepage');
+        searchButtonElement.click();
+        
+        var title = browser.getTitle();
+        console.log('Title is: ' + title);
+    });
+});
---
+++ webdriverio/wdio.conf.js
@@ -0,0 +1,14 @@
+exports.config = {
+  host: "chrome",
+  port: 9515,
+  path: "/",
+  specs: ["specs.js"],
+  capabilities: [
+    {
+      browserName: "chrome",
+      chromeOptions: {
+        debuggerAddress: "chrome:9222"
+      }
+    }
+  ]
+};
