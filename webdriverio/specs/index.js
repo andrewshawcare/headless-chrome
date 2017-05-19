6cb9c4062a2a96201ec5d37abcab633ff7cbc57c
Andrew Shaw Care <andrewshawcare@gmail.com>
2017-05-19 17:32:36 -0400

    Fix spacing

--- webdriverio/specs/index.js
+++ webdriverio/specs/index.js
@@ -4,15 +4,15 @@ const { browser } = global;
 
 describe('DuckDuckGo search', function() {
     it('searches for WebdriverIO', function() {
-        browser.url('https://duckduckgo.com/');
+      browser.url('https://duckduckgo.com/');
 
-        const searchInputElement = browser.element('#search_form_input_homepage');
-        searchInputElement.setValue('WebdriverIO');
+      const searchInputElement = browser.element('#search_form_input_homepage');
+      searchInputElement.setValue('WebdriverIO');
 
-        const searchButtonElement = browser.element('#search_button_homepage');
-        searchButtonElement.click();
+      const searchButtonElement = browser.element('#search_button_homepage');
+      searchButtonElement.click();
 
-        const firstResultTitleElement = browser.element('#r1-0');
-        assert.ok(firstResultTitleElement.getText().includes('WebdriverIO'));
+      const firstResultTitleElement = browser.element('#r1-0');
+      assert.ok(firstResultTitleElement.getText().includes('WebdriverIO'));
     });
 });
